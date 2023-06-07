c88877e1722183be5857417b3d8f5f74a51b6599
Harry R. Schwartz <harry.schwartz@testdouble.com>
2023-06-07 10:12:51 -0400

    Add GPL license boilerplate
    Submitting a package to MELPA requires that, per
    https://github.com/melpa/melpa/blob/master/CONTRIBUTING.org

--- docsim.el
+++ docsim.el
@@ -3,9 +3,22 @@
 ;; Author: Harry R. Schwartz <hello@harryrschwartz.com>
 ;; Maintainer: Harry R. Schwartz <hello@harryrschwartz.com>
 ;; Package-Requires: ((cl-lib "0.5") (org "8.0"))
-;; Homepage: https://github.com/hrs/docsim-mode
+;; Homepage: https://github.com/hrs/docsim.el
 
 ;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
